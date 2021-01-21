128589b7f745ac67ab30e5295f1f013c7b53a9e5
Patryk Plaza <atrykp0@gmail.com>
2021-01-21 07:08:30 +0100

    add draw dish function in App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "../styles/App.css";
-
+let mainArr = [
+  "Domowa pizza",
+  "Serowa petarda",
+  "Zapiekanka makaronowa z mięsem mielonym i cukinią",
+  "Kotlet z ziemniakami",
+  "Wytrawne naleśniki",
+  "Makaron z cukinią i mascarpone",
+  "Zapiekanka ziemniaczna",
+  " Zupa warzywna",
+  " Schab w sosie koperkowym",
+  "Pierogi",
+  "Krokiety po meksykańsku",
+  "Kurczak w cieście z warzywami",
+  "Gulasz",
+  " Naleśniki",
+  "Łazanki",
+  "Rosół",
+  "Chilli con carne",
+  "Zapiekanka gyros",
+  "Sałatka  z kurczakiem",
+  "Mielone",
+];
 function App() {
-  return <div className="App">Hello world</div>;
+  const [allDishes, setAllDishes] = useState(mainArr);
+  const [drawedDish, setDrawedDish] = useState("");
+  let notYetArr;
+
+  const handleDraw = () => {
+    notYetArr = [...allDishes];
+    let index = Math.floor(Math.random() * notYetArr.length);
+    setDrawedDish(notYetArr[index]);
+    notYetArr.slice(index, 1);
+    setAllDishes(notYetArr);
+  };
+  return (
+    <>
+      <button onClick={handleDraw}>Losuj</button>
+      <h1>wylosowana potrawa to:</h1>
+      <p>{drawedDish}</p>
+    </>
+  );
 }
 
 export default App;
