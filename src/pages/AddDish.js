b1cada9ddf5dbae3c8d9730dc0c5858ee389491b
Patryk Plaza <atrykp0@gmail.com>
2021-01-26 07:02:50 +0100

    add form in AddDish.js and add file AddDish.css

--- src/pages/AddDish.js
+++ src/pages/AddDish.js
@@ -1,6 +1,19 @@
 import React from "react";
+import "../styles/AddDish.css";
 
 const AddDish = () => {
-  return <div>add dish</div>;
+  return (
+    <form action="" className="addDish">
+      <input type="text" placeholder="wpisz nazwę" />
+      <input type="text" placeholder="podaj opis" />
+      <p>Podaj składniki</p>
+      <input type="text" placeholder="składnik" />
+      <input type="number" placeholder="ilość" />
+      <button>dodaj kolejny składnik</button>
+      <p>Opisz sposób przyrządzenia</p>
+      <input type="text" placeholder="opisz krok pierwszy" />
+      <button>dodaj kolejny krok</button>
+    </form>
+  );
 };
 export default AddDish;
