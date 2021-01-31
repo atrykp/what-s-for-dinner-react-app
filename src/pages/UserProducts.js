0cbcd1936fffe502f7598515e1b3d4c95220fe8c
Patryk Plaza <atrykp0@gmail.com>
2021-01-31 06:09:59 +0100

    add useState hook isActive in UserProducts.js

--- src/pages/UserProducts.js
+++ src/pages/UserProducts.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-const UserProducts = () => {
-  return <p>hello User Products</p>;
+const UserProducts = ({ allDishes, updateUserDishes }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const showSection = () => {
+    setIsActive((prevValue) => !prevValue);
+  };
+
+  return (
+    <>
+      <button onClick={showSection}>{isActive ? "ukryj" : "rozwiń"}</button>
+    </>
+  );
 };
 
 export default UserProducts;
