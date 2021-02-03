cdb0ca1e59d3d6e5185090a77a6f313d3ff580cb
Patryk Plaza <atrykp0@gmail.com>
2021-02-03 05:53:11 +0100

    create basic structure in Dish.js

--- src/pages/Dish.js
+++ src/pages/Dish.js
@@ -1,5 +1,18 @@
 import React from "react";
 const Dish = (props) => {
-  return <p>{props.selectedDish.name}</p>;
+  const { name, description, ingredient, id, ban } = props.selectedDish;
+  return (
+    <div className="selected dish">
+      <h1>{name}</h1>
+      <p className="description">{description}</p>
+      <div className="products">
+        <h2>składniki</h2>
+        <h2>Lista zakupów</h2>
+      </div>
+      <div className="Steps">
+        <h2>Przepis</h2>
+      </div>
+    </div>
+  );
 };
 export default Dish;
