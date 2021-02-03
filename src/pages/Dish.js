3b432453dd8b96e024d1b1b930c3ea8fc4df90a5
Patryk Plaza <atrykp0@gmail.com>
2021-02-03 06:08:46 +0100

    add ingredientArr and shoping list form in Dish.js

--- src/pages/Dish.js
+++ src/pages/Dish.js
@@ -1,13 +1,30 @@
 import React from "react";
 const Dish = (props) => {
   const { name, description, ingredient, id, ban } = props.selectedDish;
+  const ingredientArr = ingredient.map((item) => (
+    <li>
+      {item.name} - {item.quantity}
+    </li>
+  ));
+  const shoppingList = ingredient.map((item) => (
+    <label htmlFor={item.name}>
+      {item.name}
+      <input type="checkbox" id={item.name} />
+    </label>
+  ));
   return (
     <div className="selected dish">
       <h1>{name}</h1>
       <p className="description">{description}</p>
       <div className="products">
         <h2>składniki</h2>
+        <ul className="productsList">{ingredientArr}</ul>
         <h2>Lista zakupów</h2>
+        <p>Zaznacz co musisz jeszcze kupić</p>
+        <form action="">
+          {shoppingList}
+          <button>Zapisz</button>
+        </form>
       </div>
       <div className="Steps">
         <h2>Przepis</h2>
