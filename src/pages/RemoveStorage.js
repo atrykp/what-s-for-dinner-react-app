8fb9ecbdae641d7f4028eaae26b3ce0da0a52f36
Patryk Plaza <atrykp0@gmail.com>
2021-02-07 06:23:17 +0100

    add storageItems arr, useState hooks in RemoveStorage.js

--- src/pages/RemoveStorage.js
+++ src/pages/RemoveStorage.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
+const storageItems = [
+  "userDishes",
+  "allDishes",
+  "filterArr",
+  "userFilterArr",
+  "selectedDish",
+  "productsList",
+  "isSelected",
+];
+
 const RemoveStorage = () => {
-  return <div>co mam usunąć ? </div>;
+  const [isAccepted, setIsAccepted] = useState(false);
+  const [popupActive, setPopupActive] = useState(false);
+  const confirm = () => {
+    setPopupActive(true);
+  };
+  const popup = (
+    <>
+      <p>czy na pweno usunąć wszystkie dane aplikacji?</p>
+      <button>tak</button>
+      <button>nie</button>
+    </>
+  );
+
+  const reset = () => {
+    storageItems.forEach((item) => localStorage.removeItem(item));
+  };
+
+  return (
+    <>
+      <button onClick={confirm}>Usuń pamięć</button>
+      {popupActive && popup}
+    </>
+  );
 };
 
 export default RemoveStorage;
