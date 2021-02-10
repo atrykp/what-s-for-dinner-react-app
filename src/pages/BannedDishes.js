12aeefd71fd93c5f5077e961f95cc101ff0a1325
Patryk Plaza <atrykp0@gmail.com>
2021-02-10 07:08:57 +0100

    add banned dish list to BannedDises.js

--- src/pages/BannedDishes.js
+++ src/pages/BannedDishes.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const BannedDishes = () => {
-  return <div>banned dishes</div>;
+const BannedDishes = ({ allDishes }) => {
+  const bannedDishes = allDishes.filter((dish) => dish.ban.status);
+  const dishesList = bannedDishes.map((dish) => {
+    const dateObj = new Date(dish.ban.howLong + dish.ban.sinceWhen);
+    return (
+      <li>
+        nazwa: {dish.name} nieaktywna do: {dateObj.toLocaleString()}
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <Link to="/">Wróć do strony głównej</Link>
+      <ul className="bannedDishes">{dishesList}</ul>
+    </>
+  );
 };
 
 export default BannedDishes;
