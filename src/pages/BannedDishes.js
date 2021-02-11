71aea86d9ad8af0da4fefa4bf5efcf23c4b3fa03
Patryk Plaza <atrykp0@gmail.com>
2021-02-11 05:37:21 +0100

    add button and function removeFromBanned in BannedDishes.js

--- src/pages/BannedDishes.js
+++ src/pages/BannedDishes.js
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 
 const BannedDishes = ({ allDishes }) => {
   const bannedDishes = allDishes.filter((dish) => dish.ban.status);
+  const removeFromBanned = (id) => {
+    console.log(id);
+  };
+
   const dishesList = bannedDishes.map((dish) => {
     const dateObj = new Date(dish.ban.howLong + dish.ban.sinceWhen);
     return (
       <li>
         nazwa: {dish.name} nieaktywna do: {dateObj.toLocaleString()}
+        <button onClick={() => removeFromBanned(dish.id)}>usuń</button>
       </li>
     );
   });
@@ -15,7 +20,9 @@ const BannedDishes = ({ allDishes }) => {
   return (
     <>
       <Link to="/">Wróć do strony głównej</Link>
-      <ul className="bannedDishes">{dishesList}</ul>
+      <ul className="bannedDishes">
+        {bannedDishes.length > 0 ? dishesList : "brak"}
+      </ul>
     </>
   );
 };
