a0f7881745e286a8796958a9a9ca476b665673d2
Patryk Plaza <atrykp0@gmail.com>
2021-02-23 07:13:53 +0100

    add new dishes and change media for more menu

--- src/pages/App.js
+++ src/pages/App.js
@@ -128,6 +128,145 @@ let mainArr = [
     id: 3,
     ban: { status: false, howLong: "", sinceWhen: "" },
   },
+  {
+    name: "Spaghetti",
+    ingredient: [
+      { name: "makaron spaghetti", quantity: "300g" },
+      { name: "sos pomidorowy Dawtona", quantity: "1 słoik" },
+      { name: "mięso mielone", quantity: "500g" },
+      { name: "sól, pieprz, słodka papryka, ostra papryka", quantity: "" },
+      { name: "olej do smażenia", quantity: "" },
+      { name: "", quantity: "" },
+    ],
+    description:
+      "Spaghetti to szybkie i pożywne danie, które z pewnością gości na większości stołów. Sposobów na jego wykonanie jest wiele- można je zrobić z pomidorami z puszki, z passatą pomidorową czy też z gotowym sosem z torebki. Ja zastępuję passatę sosem pomidorowym Dawtona, który można kupić w Auchan.",
+    steps: [
+      {
+        number: 1,
+        value: "Makaron ugotój zgodnie z instrukcją na opakowaniu. ",
+      },
+      {
+        number: 2,
+        value:
+          "Na patelni rozgrzej łyżkę oleju, wrzuć mięso i smaż aż stanie się brązowe. Następnie dopraw solą, pieprzem, papryką słodką oraz ostrą.",
+      },
+      {
+        number: 3,
+        value: "Do mięsa wlej cały słoik sosu i duś jeszcze przez ok. 5min. ",
+      },
+      {
+        number: 4,
+        value: "Połącz makaron z sosem. Danie podawaj posypane parmezanem.",
+      },
+      {
+        number: 5,
+        value:
+          "Jeżeli chcesz wzbogacić swoje danie możesz dodać do sosu oliwki i pokrojoną w kostkę czerwoną paprykę.",
+      },
+    ],
+    id: 1,
+    ban: { status: false, howLong: "", sinceWhen: "" },
+  },
+  {
+    name: "Schab w sosie koperkowym",
+    ingredient: [
+      { name: "schab bez kości ", quantity: "600g" },
+      { name: "bulion drobiowy", quantity: "500 ml" },
+      { name: "marchewka", quantity: "1" },
+      { name: "cebula", quantity: "1/2" },
+      { name: "czosnek", quantity: "ząbek" },
+      { name: "śmietana 30%", quantity: "150 ml" },
+      { name: "mąka pszenna", quantity: "2 łyżki " },
+      { name: "koperek", quantity: "2 łyżki " },
+      { name: "olej", quantity: "1 łyżka" },
+      { name: "sól, pieprz", quantity: "" },
+    ],
+    description:
+      "Niesamowicie pyszne danie! Kiedy zrobiłam go po raz pierwszy polecałam go wszystkim znajomym. Może i Tobie posmakuje równie mocno jak mi ;)",
+    steps: [
+      {
+        number: 1,
+        value:
+          "Mięso kroimy w plastry i robijamy. Na patelni rozgrzewamy olej i smażymy plastry schabu przez ok. 5 min z każdej strony",
+      },
+      {
+        number: 2,
+        value:
+          "Gotujemy bulion, wlewamy go do mięsa i przyprawiamy solą i pieprzem. ",
+      },
+      {
+        number: 3,
+        value:
+          "Kiedy bulion się zagotuje zmniejszamy ogień i dusimy pod przykryciem przez godzinę. Po pół godziny dodajemy przeciśnięty przez praskę czosnek, startą marchewkę i pokrojoną w kostkę cebulę.",
+      },
+      {
+        number: 4,
+        value:
+          "Mieszamy mąkę ze śmietaną i dodajemy do mięsa. Zagotowujemy i dodajemy koperek.",
+      },
+      {
+        number: 5,
+        value:
+          "Schab przepysznie smakuje z gotowanymi ziemniakami i ulubioną surówką.",
+      },
+    ],
+    id: 1,
+    ban: { status: false, howLong: "", sinceWhen: "" },
+  },
+  {
+    name: "Zapiekanka ziemniaczana z mięsem mielonym",
+    ingredient: [
+      { name: "mięso mielone", quantity: "500g" },
+      { name: "ziemniaki", quantity: "8 sztuk" },
+      { name: "pomidory krojone ", quantity: "1 puszka" },
+      { name: "cebula", quantity: "1" },
+      { name: "czosnek", quantity: "2 ząbki" },
+      { name: "śmietana 18%", quantity: "330g" },
+      { name: "jajko", quantity: "2" },
+      { name: "ser żółty", quantity: "pół kostki" },
+      {
+        name: "sól, pieprz, słodka papryka, papryka chilli, tymianek",
+        quantity: "",
+      },
+      { name: "olej", quantity: "1 łyżka" },
+    ],
+    description:
+      "Zapiekanka ziemniaczana z mięsem mielonym świetnie sprawdzi się jako obiad na dwa dni, bo po podgrzaniu jest tak samo pyszna.",
+    steps: [
+      {
+        number: 1,
+        value:
+          "Ziemniaki obrać, pokroić w plastry i gotować przez 8 min w osolonej wodzie.",
+      },
+      {
+        number: 2,
+        value:
+          "Cebulę i czosnek pokroić w kostkę i podsmażyć na rozgrzanym oleju. Dodać mięso, smażyć, aż będzie brązowe.",
+      },
+      {
+        number: 3,
+        value:
+          "Na patelnię dodajemy puszkę pomidorów i przyprawiamy. Smażymy jeszcze przez chwilę.",
+      },
+      {
+        number: 4,
+        value:
+          "w naczyniu żaroodpornym układamy połowę ziemniaków, wykładamy na nie mięso z patelni, na mięso układamy drugą połowę ziemniaków.",
+      },
+      {
+        number: 5,
+        value:
+          "W osobnym naczyniu przygotowujemy sos. Łączymy śmietanę z jajkami i serem. Wylewamy na zapiekankę. ",
+      },
+      {
+        number: 6,
+        value:
+          "Zapiekankę wstawiamy do nagrzanego do 180 st. piekarnika i pieczemy przez ok 30 min. Do całkowitej miękkości ziemniaków. Pod koniec pieczenia można włączyć termoobieg, żeby przypiec sos z wierzchu.",
+      },
+    ],
+    id: 1,
+    ban: { status: false, howLong: "", sinceWhen: "" },
+  },
 ];
 
 function App() {
